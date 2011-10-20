ed41488b469cd23296d672914e45312d34573bc2
Daryl <daryl@commoncreative.com.au>
2011-10-20 14:14:49 +1200

    Update django/utils/feedgenerator.py

--- django/utils/feedgenerator.py
+++ django/utils/feedgenerator.py
@@ -269,7 +269,10 @@ class Rss201rev2Feed(RssFeed):
         if item['comments'] is not None:
             handler.addQuickElement(u"comments", item['comments'])
         if item['unique_id'] is not None:
-            handler.addQuickElement(u"guid", item['unique_id'])
+            if item['link'] is not None:
+                handler.addQuickElement(u"guid", item['unique_id'], {u"isPermaLink": "false"})
+            else:
+                handler.addQuickElement(u"guid", item['unique_id'])
         if item['ttl'] is not None:
             handler.addQuickElement(u"ttl", item['ttl'])
 
